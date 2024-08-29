953fb1654861f876b08949d934500c981730c838
Rafy Bhuiyan <rafybhuiyan55@gmail.com>
2024-08-29 17:53:07 +0600

    Firebase resolved

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    name: 'sahajyo-syntax',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
-void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -123,3 +129,4 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
